ff31d885f484317ec49bd02feaaae96a1e9e0bfd
Sinisteroid <ghadamgahi.omid@gmail.com>
2023-02-27 14:16:21 +0100

    hm

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.post('/api/addpost', (req, res) => {
 /* -------------------------------------------------------------------------- */
 
 app.get('/api/last', async(request,response) => {
-    const lastFmUri= 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=simplyeffedup&limit=10&api_key=f59084f6f83edbe693185eb0ec6b2272&format=json';
+    const lastFmUri= 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=simplyeffedup&limit=10&api_key=f59084f6f83edbe693185eb0ec6b2272';
     const fetch_response = await fetch(lastFmUri);
     const json = await fetch_response.json();
     response.json(json);
