3c10e203771f104fba2023920f7c8bc085dde98b
Sinisteroid <ghadamgahi.omid@gmail.com>
2023-03-02 06:44:20 +0300

    hm

--- server.js
+++ server.js
@@ -1,31 +1,18 @@
 const express = require('express');
 const app = express();
-require('dotenv').config();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const Cors = require("cors");
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-
-
-app.use(Cors());
-
-const xss = require('xss-clean')
-const mongoSanitize = require('express-mongo-sanitize');
-
-const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority
-`
+const mongoUri = "mongodb+srv://Sinisteroid:XEplQ9B3i7TX97J9@sinisteroid0.8biscsu.mongodb.net/musicPostsDB?retryWrites=true&w=majority"
+const lastUri = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=simplyeffedup&limit=3&api_key=f59084f6f83edbe693185eb0ec6b2272&format=json'
 
 
 /* -------------------------- Get rid of the error -------------------------- */
 mongoose.set('strictQuery', false);
 /* -------------------------------------------------------------------------- */
 mongoose.connect(mongoUri);
-//Parsing
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
-//Sanitize
-app.use(xss());
-app.use(mongoSanitize());
 
 /* -------------------------------------------------------------------------- */
 /*                                   Schema                                   */
@@ -52,12 +39,24 @@ const Post = mongoose.model('Post', postSchema)
 /*                                  Get Posts                                 */
 /* -------------------------------------------------------------------------- */
 
-app.get('/api/getposts',(req,res)=>{
-    Post.find({},null, {sort: {_id: -1}},(err,doc)=>{
-        if(err) return console.log(err);
+app.get('/api/getposts', (req, res) => {
+    Post.find({}, null, { sort: { _id: -1 } }, (err, doc) => {
+        if (err) return console.log(err);
         res.json(doc)
     })
 })
+
+/* -------------------------------------------------------------------------- */
+/*                                   lastFm                                   */
+/* -------------------------------------------------------------------------- */
+
+app.get('/api/last', async(request,response) => {
+    const lastFmUri= 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=simplyeffedup&limit=10&api_key=f59084f6f83edbe693185eb0ec6b2272&format=json';
+    const fetch_response = await fetch(lastFmUri);
+    console.log(fetch_response)
+    const json = await fetch_response.json();
+    response.json(json);
+});
 
 /* -------------------------------------------------------------------------- */
 /*                                  Add Posts                                 */
@@ -87,22 +86,9 @@ app.post('/api/addpost', (req, res) => {
 /*                                   Last.fm                                  */
 /* -------------------------------------------------------------------------- */
 
-
+const lastFmUri = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=simplyeffedup&limit=10&api_key=f59084f6f83edbe693185eb0ec6b2272&format=json';
 
 //Port
-
-app.use(express.static('client/build'));
-if(process.env.NODE_ENV === 'production'){
-    const path = require('path');
-
-    app.get('/*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'./client','build','index.html'))
-    })
-}
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-}
 
 const port = process.env.POST || 3001;
 app.listen(port);
